d6097661bb34f66afa7f41bfa457e0ab201a8f28
Osva Fernandez <osvanaranjo96@gmail.com>
2026-01-10 14:39:17 -0300

    add map endpoints

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
+using System.Net.Http.Headers;
 using Scalar.AspNetCore;
+using webapiAI.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,5 +17,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapApiEndpoints();
 
 app.Run();
