ae84e7e968c131b7a273ae7f6a7d92bf031234c4
Osva Fernandez <osvanaranjo96@gmail.com>
2026-01-11 02:06:18 -0300

    Create ModelService.cs

---
+++ Common/Entities/ModelService.cs
@@ -0,0 +1,9 @@
+namespace webapiAI.Common.Entities;
+public class ModelService
+{
+    public string Uri { get; set; } = "https://api.groq.com/openai/v1/chat/completions";
+
+    public string Model { get; set; } = "moonshotai/kimi-k2-instruct-0905";
+
+    public int MaxTokens { get; set; } = 4096;
+}
