f0b179c9055688530c54eb8ba6a1513f8d67201b
Osva Fernandez <osvanaranjo96@gmail.com>
2026-01-12 16:55:06 -0300

    add ModelService to balancer

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Scalar.AspNetCore;
 using webapiAI.Common;
 using webapiAI.Endpoints;
+using webapiAI.Common.Entities;
 
 DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
@@ -9,23 +10,38 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var services = new List<webapiAI.Common.Entities.ModelService>
+var services = new List<ModelService>
 {
-    new webapiAI.Common.Entities.ModelService
+    new ModelService
+    {
+        Uri = "https://openrouter.ai/api/v1/chat/completions",
+        Model = "openai/gpt-5.2",
+        ApiKey = Environment.GetEnvironmentVariable("OPEN_ROUTER_KEY") ?? 
+            builder.Configuration["OPEN_ROUTER_KEY"] ?? throw new InvalidOperationException("OPEN_ROUTER_KEY was not found in environment variables or .env."),
+        MaxTokens = 2857
+    },
+    new ModelService
     {
         Uri = "https://api.groq.com/openai/v1/chat/completions",
         Model = "moonshotai/kimi-k2-instruct-0905",
-        ApiKey = Environment.GetEnvironmentVariable("GROK_API_KEY")
+        ApiKey = Environment.GetEnvironmentVariable("GROK_API_KEY") ?? builder.Configuration["GROQ_API_KEY"]
             ?? throw new InvalidOperationException("GROK_API_KEY (or GROQ_API_KEY) was not found in environment variables or .env."),
         MaxTokens = 4096
     },
-    new webapiAI.Common.Entities.ModelService
+    new ModelService
     {
         Uri = "https://api.cerebras.ai/v1/chat/completions",
         Model = "llama-3.3-70b",
-        ApiKey = Environment.GetEnvironmentVariable("CEREBRAS_API_KEY")
-            ?? throw new InvalidOperationException("CEREBRAS_API_KEY was not found in environment variables or .env."),
+        ApiKey = Environment.GetEnvironmentVariable("CEREBRAS_API_KEY") ?? 
+            builder.Configuration["CEREBRAS_API_KEY"] ?? throw new InvalidOperationException("CEREBRAS_API_KEY was not found in environment variables or .env."),
         MaxTokens = 8192
+    },
+    new ModelService
+    {
+        Uri = "https://api.groq.com/openai/v1/chat/completions",
+        Model = "openai/gpt-oss-120b",
+        ApiKey = Environment.GetEnvironmentVariable("GROQ_OSVANARANJO_API_KEY") ?? 
+            builder.Configuration["GROQ_OSVANARANJO_API_KEY"] ?? throw new InvalidOperationException("GROQ_OSVANARANJO_API_KEY was not found in environment variables or .env."),
     }
 };
 
